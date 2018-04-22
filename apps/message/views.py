61fd17d948400f75a8f3368143875d73099ae7e2
lbxz <957062024@qq.com>
2018-04-22 17:46:11 +0800

    test提交代码

--- apps/message/views.py
+++ apps/message/views.py
@@ -1,6 +1,20 @@
 # coding:utf-8
 from django.shortcuts import render
-
+from .models import UserMessage
 
 def getform(request):
+    if request.method == "POST":
+        name = request.POST.get('name', '')
+        message = request.POST.get('message', '')
+        address = request.POST.get('address', '')
+        email = request.POST.get('email', '')
+
+        user_messge = UserMessage()
+        user_messge.name = name
+        user_messge.message = message
+        user_messge.address = address
+        user_messge.email = email
+
+        user_messge.save()
+
     return render(request, 'message_form.html')
